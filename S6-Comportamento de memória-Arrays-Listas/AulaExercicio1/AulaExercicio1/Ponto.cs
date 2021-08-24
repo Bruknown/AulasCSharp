40e5b34c6f9d985b9babf4733e7b0ff26b5256df
Bruknown <brunobrandini77@gmail.com>
2021-08-24 17:40:23 -0300

    inicio da seção 6

---
+++ S6-Comportamento de memória-Arrays-Listas/AulaExercicio1/AulaExercicio1/Ponto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AulaExercicio1
+{
+    struct Ponto
+    {
+        public double x;
+        public double y;
+
+        public override string ToString()
+        {
+            return "(" + x + "," + y + ")";
+        }
+    }
+}
---
+++ S6-Comportamento de memória-Arrays-Listas/AulaExercicio1/AulaExercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AulaExercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto p;
+            p.x = 10;
+            p.y = 20;
+            Console.WriteLine(p);
+        }
+    }
+}
