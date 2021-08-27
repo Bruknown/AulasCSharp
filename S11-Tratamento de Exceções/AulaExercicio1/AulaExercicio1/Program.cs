5c300385d1a95d874ada96ad6e9a86259a4a48d2
Bruknown <brunobrandini77@gmail.com>
2021-08-27 13:26:24 -0300

    S-11 completo

---
+++ S11-Tratamento de Exceções/AulaExercicio1/AulaExercicio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using AulaExercicio1.Entities;
+using System;
+
+namespace AulaExercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check in date dd/mm/yyyy: ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Check out date dd/mm/yyyy: ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            if (checkout <= checkin)
+            {
+                Console.WriteLine("erro na data, check in não pode ser depois de checkout")
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+        }
+    }
+}
