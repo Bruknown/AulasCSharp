2ae976de3e6c98492294a7386bfadbdc3117627f
Bruknown <brunobrandini77@gmail.com>
2021-09-17 17:55:34 -0300

    componentes Listbox2

---
+++ FORMS1-Primeiro Modulo/ProjetoModulo1/ProjetoModulo1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ProjetoModulo1
+{
+    partial class form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lblNomeSaida = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(308, 324);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(227, 82);
+            this.btnLimpar.TabIndex = 0;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(75, 324);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(227, 82);
+            this.btnCadastrar.TabIndex = 1;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnBotao1_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(12, 21);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(57, 20);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome: ";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(75, 18);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(460, 27);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtNome_KeyUp);
+            // 
+            // lblNomeSaida
+            // 
+            this.lblNomeSaida.AutoSize = true;
+            this.lblNomeSaida.Location = new System.Drawing.Point(12, 71);
+            this.lblNomeSaida.Name = "lblNomeSaida";
+            this.lblNomeSaida.Size = new System.Drawing.Size(39, 20);
+            this.lblNomeSaida.TabIndex = 4;
+            this.lblNomeSaida.Text = "Lista";
+            this.lblNomeSaida.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(75, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(460, 244);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 428);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblNomeSaida);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.btnLimpar);
+            this.Name = "form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblNomeCopiado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lblNomeSaida;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ FORMS1-Primeiro Modulo/ProjetoModulo1/ProjetoModulo1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoModulo1
+{
+    public partial class form : Form
+    {
+        private int iSelecionado;
+
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBotao1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(txtNome.Text);
+            txtNome.Text = String.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            txtNome.Text = String.Empty;
+        }
+
+        private void txtNome_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnBotao1_Click(btnCadastrar, new EventArgs());
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            iSelecionado = listBox1.SelectedIndex;
+            txtNome.Text = ListBox1.Items[iSelecionado];
+            btnCadastrar.Text = "Alterar";
+        }
+    }
+}
